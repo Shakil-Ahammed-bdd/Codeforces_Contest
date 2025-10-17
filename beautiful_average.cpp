404d4b48975bfb435f8280df20d5f26791ec193a
Md.Shakil Ahammed <shajadulhasanbd@gmail.com>
2025-10-18 00:24:56 +0600

    Beautiful Average

--- beautiful_average.cpp
+++ beautiful_average.cpp
@@ -3,15 +3,22 @@ using namespace std;
 using ll = long long;
 
 void solve(){
-
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0; i<n; i++){
+    cin>>a[i];
+  }
+  int maxx = *max_element(a,a+n);
+  cout<<maxx<<endl;
 
 }
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);cout.tie(NULL);
-  // int t; cin>>t;
+  int t; cin>>t;
 
-  // while(t--){
+  while(t--){
     solve();
-  // }
+  }
 }
