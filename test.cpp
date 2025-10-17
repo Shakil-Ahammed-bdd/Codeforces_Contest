f67a3d2a712600a8e3514a12da4f60e247e8ae13
Md.Shakil Ahammed <shajadulhasanbd@gmail.com>
2025-10-17 07:48:53 +0600

    first commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve(){
+  cout<<"Hello"<<endl;
+  cout<<"Shakil"<<endl;
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);cout.tie(NULL);
+  // int t; cin>>t;
+
+  // while(t--){
+    solve();
+  // }
+}
